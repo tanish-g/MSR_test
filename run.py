92d97ae087a7d9846fe3085fe6c0add801c0178c
Taneesh Gupta <55942819+tanish-g@users.noreply.github.com>
2023-08-05 20:48:13 +0530

    Update run.py

--- run.py
+++ run.py
@@ -61,7 +61,7 @@ def correct_function_call(code,caller_name, callee_name, loglevel,argument):
     corrected_call = response.choices[0].text.strip()
     return corrected_call
 
-def main(repo_path,cuda,required_callee,loglevel):
+def main(repo_path,required_callee,loglevel):
     argument1 = "Add loglevel argument in Caller and  display only the modified code not caller function"
     argument2 = "Add loglevel argument in Callee inside Caller and  display only the modified code not caller function"
     
@@ -77,27 +77,28 @@ def main(repo_path,cuda,required_callee,loglevel):
     PY_LANGUAGE = Language('build/my-languages.so', 'python')
     parser = Parser()
     parser.set_language(PY_LANGUAGE)
+    
     for root, _, files in os.walk(repo_path):
         for file in files:
             if file.endswith('.py'):
-                
-                print(os.path.join(root, file))
-
                 with open(os.path.join(root, file), 'r') as f:
                     code = f.read()
+
+                with open(os.path.join(root.replace('original','modified'), file), 'r') as f:
+                    correct_code = f.read()
                 
                 # Step 1: Parse the tree
                 tree = parser.parse(bytes(code, 'utf-8'))
                 root_node = tree.root_node
                 
                 # Step 2: Identify caller-callee relations between functions
-                global caller_callee_relations
-                caller_callee_relations = {}
+                global caller_callee_relation
+                caller_callee_relation = {}
                 traverse_ast_rc(root_node,None,required_callee)
                 
                 # Step 3: Use the LLM to modify the target function and function calls in the other classes as well
 
-                for caller,callees in caller_callee_relations.items():
+                for caller,callees in caller_callee_relation.items():
                    if len(callees)==0 and caller.decode('ASCII')==required_callee:
                       modified_code = correct_function_call(code,caller.decode('ASCII'),None,loglevel,argument1)
                    for callee in callees:
@@ -109,7 +110,7 @@ def main(repo_path,cuda,required_callee,loglevel):
                     f.write(modified_code)
                 
                 #Step5: Evaluate the correctness whether the modified code matches the generated code
-                score = evaluate_correctness(code,modified_code)
+                score = evaluate_correctness(correct_code,modified_code)
 
 
 if __name__ == "__main__":
@@ -117,10 +118,9 @@ if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
 
     arg_parser.add_argument("--repo_path", type=str, default=".")
-    arg_parser.add_argument("--corr_repo_path",type=str,default=".")
     arg_parser.add_argument("--required_callee", type=str, default="log")
     arg_parser.add_argument("--loglevel", type=str, default="debug")
 
     args = arg_parser.parse_args()
 
-    main(args.repo_path,args.corr_repo_path,args.required_callee,args.loglevel)
+    main(args.repo_path,args.required_callee,args.loglevel)
