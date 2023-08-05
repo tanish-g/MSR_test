5d1d918494266169ea964aa40a53b787e6645559
Taneesh Gupta <55942819+tanish-g@users.noreply.github.com>
2023-08-05 18:49:28 +0530

    Create metrics.py

---
+++ example1/modified/utils/metrics.py
@@ -0,0 +1,16 @@
+from sklearn.metrics import mean_squared_error, f1_score, log_loss,mean_absolute_error,accuracy_score
+class composed_metrics():
+  
+  def __init__(self,handler):
+    self.handler = handler
+
+  def mae(self,y_true, y_pred):
+      score = mean_absolute_error(y_true, y_pred) # RMSE
+      self.handler.logger('mae : ' + str(score),loglevel='debug')
+      return score
+
+
+  def accuracy(self,y_true, y_pred):
+      score = accuracy_score(y_true, y_pred)
+      self.handler.logger('accuracy : ' + str(score),loglevel='debug')
+      return score
