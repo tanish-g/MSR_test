b602da677c85062ae58aadf0026dbb7a3b1d0f14
Taneesh Gupta <55942819+tanish-g@users.noreply.github.com>
2023-08-05 18:51:38 +0530

    Update logger.py

--- example1/modified/utils/logger.py
+++ example1/modified/utils/logger.py
@@ -2,5 +2,5 @@ class Handler():
   def __init__(self,filename):
      self.filename = filename
   
-  def logger(self,string,loglevel='debug'):
+  def log(self,string,loglevel='debug'):
      print(str, ' loglevel : ', loglevel)
