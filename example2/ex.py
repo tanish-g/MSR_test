986d9b6d2eeb8477e53b5d4aa41865569c5a172d
Taneesh Gupta <55942819+tanish-g@users.noreply.github.com>
2023-08-05 19:22:35 +0530

    Create ex.py

---
+++ example2/ex.py
@@ -0,0 +1,15 @@
+class rep():
+  def log(str):
+    print(str)
+  
+  def test():
+    text = 'Started Testing of Model'
+    log(text)
+  
+  def train():
+    text = 'Started Training of Model'
+    log(text)
+  
+  def comb():
+    train()
+    test()
