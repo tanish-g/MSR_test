6cd91747ae780de0156a536be2d2b14a04123d77
Taneesh Gupta <55942819+tanish-g@users.noreply.github.com>
2023-08-05 18:43:33 +0530

    Update metrics.py

--- example1/original/utils/metrics.py
+++ example1/original/utils/metrics.py
@@ -1,6 +1,16 @@
-class Handler():
-  def __init__(self,filename):
-    self.filename = filename
+from sklearn.metrics import mean_squared_error, f1_score, log_loss,mean_absolute_error,accuracy_score
+
+class composed_metrics():
   
-  def logger(self,string):
-    print(string)
+  def __init__(self,handler):
+    self.handler = handler
+
+  def mae(self,y_true, y_pred):
+      score = mean_absolute_error(y_true, y_pred) # RMSE
+      self.handler.logger('mae : ' + str(score))
+      return score
+
+  def accuracy(self,y_true, y_pred):
+      score = accuracy_score(y_true, y_pred)
+      self.handler.logger('accuracy : ' + str(score))
+      return score
