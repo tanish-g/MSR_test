04c686f1f6df24e6f6b0aa68395c2838abe38a8a
Taneesh Gupta <55942819+tanish-g@users.noreply.github.com>
2023-08-05 18:40:21 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+pip install tree-sitter
+pip install openai
+pip install transformers
+pip install evaluate
+git clone https://github.com/tree-sitter/tree-sitter-go
+git clone https://github.com/tree-sitter/tree-sitter-javascript
+git clone https://github.com/tree-sitter/tree-sitter-python
